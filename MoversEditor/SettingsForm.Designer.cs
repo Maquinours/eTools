1d285d0fafa2f9214044698fc018604b9abf7048
Maquinours <79869121+Maquinours@users.noreply.github.com>
2023-01-22 21:51:05 +0100

    Icons added

--- MoversEditor/SettingsForm.Designer.cs
+++ MoversEditor/SettingsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
             this.label1 = new System.Windows.Forms.Label();
             this.tb_ResourcesPath = new System.Windows.Forms.TextBox();
             this.tb_PropFileName = new System.Windows.Forms.TextBox();
@@ -133,6 +134,7 @@
             this.Controls.Add(this.tb_PropFileName);
             this.Controls.Add(this.tb_ResourcesPath);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SettingsForm";
             this.Text = "Paramètres";
             this.ResumeLayout(false);
