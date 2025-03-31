c0a3a15bc621ca95082a6e108cfd7b40afd7344b
Maquinours <juju9559@hotmail.fr>
2025-03-31 17:00:58 +0200

    Ready to publish

--- MoversEditor/Properties/AssemblyInfo.cs
+++ MoversEditor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("MoversEditor")]
+[assembly: AssemblyTitle("eTools Movers Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MoversEditor")]
-[assembly: AssemblyCopyright("Copyright © Maqui - 2023")]
+[assembly: AssemblyProduct("eTools Movers Editor")]
+[assembly: AssemblyCopyright("Copyright © Maqui - 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
