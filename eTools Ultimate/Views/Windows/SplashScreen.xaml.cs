5047b529df92b166a774af685ffc2405f4d62f61
Kiyo <51216258+Kiyoshi-oe@users.noreply.github.com>
2025-04-20 18:42:36 +0200

    Added Splash Screen Page
    Splash screen page
    Show: Tool Version, Loading text & Progress bar

---
+++ eTools Ultimate/Views/Windows/SplashScreen.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace eToolsUltimate.Views.Windows
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+} 
