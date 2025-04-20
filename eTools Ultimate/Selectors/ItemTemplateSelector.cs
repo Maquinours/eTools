b530d448ce16d529f8e896767a92e9bd53f1932b
Maquinours <juju9559@hotmail.fr>
2025-04-20 10:28:12 +0200

    Ultimate Item templates systems

---
+++ eTools Ultimate/Selectors/ItemTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace eTools_Ultimate.Selectors
+{
+    public class ItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CommonTemplate { get; set; }
+        public DataTemplate EquipmentTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return EquipmentTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
