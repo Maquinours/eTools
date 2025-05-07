9902e289500f289861a064b14d93d1ef20d46fc5
Maquinours <juju9559@hotmail.fr>
2025-05-07 17:01:50 +0200

    Couple loading (not finished)

---
+++ eTools Ultimate/Models/Couple.cs
@@ -0,0 +1,170 @@
+﻿using eTools_Ultimate.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTools_Ultimate.Models
+{
+    public class CoupleLevelItem(string nItemId, string nSex, int nFlags, int nLife, int nNum) : INotifyPropertyChanged
+    {
+        private string _nItemId = nItemId;
+        private string _nSex = nSex;
+        private int _nFlags = nFlags;
+        private int _nLife = nLife;
+        private int _nNum = nNum;
+
+        public string NItemId
+        {
+            get => this._nItemId;
+            set
+            {
+                if(this.NItemId != value)
+                {
+                    this._nItemId = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public string NSex
+        {
+            get => this._nSex;
+            set
+            {
+                if(this.NSex != value)
+                {
+                    this._nSex = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public int NFlags
+        {
+            get => this._nFlags;
+            set
+            {
+                if(this.NFlags != value)
+                {
+                    this._nFlags = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public int NLife
+        {
+            get => this._nLife;
+            set
+            {
+                if(this.NLife != value)
+                {
+                    this._nLife = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public int NNum
+        {
+            get => this._nNum;
+            set
+            {
+                if(this.NNum != value)
+                {
+                    this._nNum = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CoupleLevelSkill(string nSkillKind, int nSkillLevel) : INotifyPropertyChanged
+    {
+        private string _nSkillKind = nSkillKind;
+        private int _nSkillLevel = nSkillLevel;
+
+        public string NSkillKind
+        {
+            get => this._nSkillKind;
+            set
+            {
+                if(this.NSkillKind != value)
+                {
+                    this._nSkillKind = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int NSkillLevel
+        {
+            get => this._nSkillLevel;
+            set
+            {
+                if(this.NSkillLevel != value)
+                {
+                    this._nSkillLevel = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public CoupleLevelSkill Clone()
+        {
+            return new CoupleLevelSkill(nSkillKind: this.NSkillKind, nSkillLevel: this.NSkillLevel);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CoupleLevel(int nExp, List<CoupleLevelItem> items) : INotifyPropertyChanged
+    {
+        private int _nExp = nExp;
+        private readonly ObservableCollection<CoupleLevelItem> _items = new ObservableCollection<CoupleLevelItem>(items);
+
+        public int NExp
+        {
+            get => this._nExp;
+            set
+            {
+                if(this.NExp != value)
+                {
+                    this._nExp = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<CoupleLevelItem> Items => this._items;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class Couple
+    {
+        private readonly List<CoupleLevel> _levels;
+        private readonly List<string> _skillKinds;
+
+        public List<CoupleLevel> Levels => this._levels;
+        public List<string> SkillKinds => this._skillKinds;
+    }
+}
--- eTools Ultimate/Services/CoupleService.cs
+++ eTools Ultimate/Services/CoupleService.cs
@@ -1,30 +1,157 @@
 using eTools_Ultimate.Models;
+using Scan;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using eTools_Ultimate.Exceptions;
+using eTools_Ultimate.Helpers;
 
 namespace eTools_Ultimate.Services
 {
     public class CoupleService
     {
-        private static CoupleService _instance;
-        public static CoupleService Instance => _instance ??= new CoupleService();
+        private static Lazy<CoupleService> _instance = new(() => new());
+        public static CoupleService Instance => _instance.Value;
 
-        // Level-Anforderungen aus der couple.inc-Datei
-        public ObservableCollection<int> LevelRequirements { get; private set; } = new ObservableCollection<int>();
-
-        // Items aus der couple.inc-Datei
-        public ObservableCollection<CoupleItem> CoupleItems { get; private set; } = new ObservableCollection<CoupleItem>();
-
-        // Skills aus der couple.inc-Datei
-        public ObservableCollection<CoupleSkill> CoupleSkills { get; private set; } = new ObservableCollection<CoupleSkill>();
+        private readonly ObservableCollection<CoupleLevel> _coupleLevels = [];
+        public ObservableCollection<CoupleLevel> CoupleLevels => this._coupleLevels;
 
         private CoupleService()
         {
             // Initialisierung mit Dummy-Daten
             InitializeDummyData();
+        }
+
+        public void Load()
+        {
+            Settings settings = Settings.Instance;
+            ObservableDictionary<string, int> defines = DefinesService.Instance.Defines;
+
+            // TODO: replace with settings prop
+            string filePath = $"{settings.ResourcesFolderPath}couple.inc";
+
+            using(Scanner scanner = new())
+            {
+                scanner.Load(filePath);
+
+                List<int> exp = [];
+                Dictionary<int, List<CoupleLevelItem>> items = []; // Couple level to items
+                Dictionary<int, List<CoupleLevelSkill>> skills = []; // Couple level to skills
+                List<string> skillKinds = [];
+
+                while(true)
+                {
+                    scanner.GetToken();
+
+                    if (scanner.EndOfStream) break;
+
+                    switch(scanner.Token)
+                    {
+                        case "Level":
+                            {
+                                scanner.GetToken(); // {
+                                while(true)
+                                {
+                                    int nExp = scanner.GetNumber();
+
+                                    if (scanner.Token == "}") break;
+                                    if (scanner.EndOfStream) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    exp.Add(nExp);
+                                }
+                                break;
+                            }
+                        case "Item":
+                            {
+                                scanner.GetToken(); // {
+                                while(true)
+                                {
+                                    int nLevel = scanner.GetNumber();
+
+                                    if (nLevel < 1 || nLevel > exp.Count) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    if (!items.ContainsKey(nLevel))
+                                        items[nLevel] = [];
+
+                                    if (scanner.Token == "}") break;
+                                    if (scanner.EndOfStream) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    scanner.GetToken(); // {
+                                    while(true)
+                                    {
+                                        string nItemId = scanner.GetToken();
+
+                                        if (scanner.Token == "}") break;
+                                        if (scanner.EndOfStream) throw new IncorrectlyFormattedFileException(filePath);
+
+                                        string nSex = scanner.GetToken();
+                                        int nFlags = scanner.GetNumber();
+                                        int nLife = scanner.GetNumber();
+                                        int nNum = scanner.GetNumber();
+
+                                        CoupleLevelItem item = new(nItemId: nItemId, nSex: nSex, nFlags: nFlags, nLife: nLife, nNum: nNum);
+                                        items[nLevel - 1].Add(item);
+                                    }
+                                }
+                                break;
+                            }
+                        case "SkillKind":
+                            {
+                                scanner.GetToken(); // {
+                                while (true)
+                                {
+                                    string nSkill = scanner.GetToken();
+
+                                    if (scanner.Token == "}") break;
+                                    if (scanner.EndOfStream) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    skillKinds.Add(nSkill);
+                                }
+                                break;
+                            }
+                        case "SkillLevel":
+                            {
+                                scanner.GetToken(); // {
+                                
+                                while(true)
+                                {
+                                    int nLevel = scanner.GetNumber();
+
+                                    if (scanner.Token == "}") break;
+                                    if (scanner.EndOfStream) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    if (nLevel < 1 || nLevel > exp.Count) throw new IncorrectlyFormattedFileException(filePath);
+
+                                    if (!skills.ContainsKey(nLevel))
+                                        skills[nLevel] = [];
+
+                                    foreach (string skillKind in skillKinds)
+                                    {
+                                        int nSkillLevel = scanner.GetNumber();
+                                        //int nSkill = 0;
+                                        //if(nSkillLevel > 0)
+                                        //    nSkill = defines[skillKinds[i]] + nLevel;
+                                        //string skill = defines.Where(x => x.Key.StartsWith("II_") && x.Value == nSkill).First().Key;
+                                        CoupleLevelSkill coupleLevelSkill = new(skillKind, nSkillLevel);
+                                        skills[nLevel - 1].Add(coupleLevelSkill);
+                                    }
+                                }
+
+                                if (!skills.ContainsKey(0)) throw new IncorrectlyFormattedFileException(filePath);
+
+                                for(int i = 1; i < exp.Count; i++)
+                                {
+                                    if (!skills.ContainsKey(i))
+                                        skills.Add(i, [.. skills[i - 1].Select(x => x.Clone())]);
+                                }
+
+                                break;
+                            }
+                    }
+                }
+            }
         }
 
         /// <summary>
