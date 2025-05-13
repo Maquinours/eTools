4be10c3bac946021a0253554818d6df6f2d96427
Maquinours <juju9559@hotmail.fr>
2025-05-13 23:55:00 +0200

    Movers ModelViewer setup

---
+++ eTools Ultimate/Helpers/NativeMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTools_Ultimate.Helpers
+{
+    internal static class NativeMethods
+    {
+        [DllImport("3DModelRenderer.dll")]
+        public static extern IntPtr CreateModelViewer();
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern bool InitializeEnvironment(IntPtr hwnd, IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern int GetSurfaceWidth(IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern int GetSurfaceHeight(IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern void Render(IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern IntPtr GetSharedSurface(IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern bool LoadModel(IntPtr engine, string path);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern int GetNumMotions(IntPtr engine);
+
+        [DllImport("3DModelRenderer.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr GetMotionName(IntPtr engine, int index);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern void PlayMotion(IntPtr engine, int index);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern void RotateCamera(IntPtr engine, int x, int y);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern void ZoomCamera(IntPtr engine, float delta);
+
+        [DllImport("3DModelRenderer.dll")]
+        public static extern void ResizeViewport(IntPtr engine, int width, int height);
+    }
+}
--- eTools Ultimate/Views/Pages/Mover/MoverPage.xaml.cs
+++ eTools Ultimate/Views/Pages/Mover/MoverPage.xaml.cs
@@ -3,12 +3,20 @@ using System.Windows.Controls;
 using Wpf.Ui.Controls;
 using Wpf.Ui;
 using Wpf.Ui.Abstractions.Controls;
+using eTools_Ultimate.Helpers;
+using System.Windows.Interop;
+using System.Windows.Media;
+using eTools_Ultimate.Models;
 
 namespace eTools_Ultimate.Views.Pages
 {
     public partial class MoverPage : Page, INavigableView<MoversViewModel>
     {
         public MoversViewModel ViewModel { get; }
+
+        private D3DImageHost? _d3dHost = null;
+        private Point lastMousePosition;
+        private bool isDragging = false;
 
         public MoverPage(MoversViewModel viewModel)
         {
@@ -69,6 +77,62 @@ namespace eTools_Ultimate.Views.Pages
                 
                 System.Console.WriteLine($"Error: {ex.Message}\n{ex.StackTrace}");
             }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(Window.GetWindow(this)).Handle;
+            _d3dHost = new D3DImageHost(hwnd);
+            _d3dHost.Initialize(hwnd);
+            _d3dHost.BindBackBuffer();
+            DxImage.Source = _d3dHost;
+
+            CompositionTarget.Rendering += (s, e) => _d3dHost.Render();
+        }
+
+        private void FileTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_d3dHost is null) return;
+            if (ViewModel.MoversView.CurrentItem is not Mover mover) return;
+            NativeMethods.LoadModel(_d3dHost._native, @$"{Settings.Instance.ResourcesFolderPath}Model\mvr_{mover.Model.SzName}.o3d");
+        }
+
+        private void DxImage_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var currentPosition = e.GetPosition(null);
+            isDragging = true;
+            lastMousePosition = currentPosition;
+        }
+
+        private void Page_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (_d3dHost is null) return;
+            if (!isDragging) return;
+            
+            Point mousePosition = e.GetPosition(null);
+            Vector deltaPosition = lastMousePosition - mousePosition;
+
+            int w = NativeMethods.GetSurfaceWidth(_d3dHost._native);
+            int h = NativeMethods.GetSurfaceHeight(_d3dHost._native);
+
+            //double transformX = w / DxImage.ActualWidth;
+            //double transformY = h / DxImage.ActualHeight;
+
+            NativeMethods.RotateCamera(_d3dHost._native, (int)(deltaPosition.X), (int)(deltaPosition.Y));
+
+            lastMousePosition = mousePosition;
+        }
+
+        private void Page_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void DxImage_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (_d3dHost is null) return;
+            NativeMethods.ZoomCamera(_d3dHost._native, e.Delta);
+            e.Handled = true;
         }
     }
 } 
