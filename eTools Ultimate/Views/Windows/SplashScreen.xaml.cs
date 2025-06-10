f84c6fcca39632f644fc0825a6c829d95b805d1a
Maquinours <juju9559@hotmail.fr>
2025-06-10 15:45:16 +0200

    Loading TaskBar Progress

--- eTools Ultimate/Views/Windows/SplashScreen.xaml.cs
+++ eTools Ultimate/Views/Windows/SplashScreen.xaml.cs
@@ -12,115 +12,43 @@ namespace eTools_Ultimate.Views.Windows
 
         public async Task Load()
         {
+            (string text, Action loader)[] loadingSteps = [
+                ("Loading settings...", SettingsService.Load),
+                ("Loading defines...", DefinesService.Instance.Load),
+                ("Loading strings...", StringsService.Instance.Load),
+                ("Loading models...", ModelsService.Instance.Load),
+                ("Loading items...", ItemsService.Instance.Load),
+                ("Loading movers...", MoversService.Instance.Load),
+                ("Loading skills...", SkillsService.Instance.Load),
+                ("Loading characters...", CharactersService.Instance.Load),
+                ("Loading sounds config...", SoundsService.Instance.Load),
+                ("Loading texts...", TextsService.Instance.Load),
+                ("Loading giftboxes...", GiftBoxesService.Instance.Load),
+                ("Loading exchanges...", ExchangesService.Instance.Load),
+                ("Loading honors...", HonorsService.Instance.Load),
+                ("Loading motions...", MotionsService.Instance.Load),
+                ("Loading accessories...", AccessoriesService.Instance.Load),
+                ("Loading couple configuration...", CoupleService.Instance.Load),
+                ("Loading tickets...", TicketsService.Instance.Load),
+                ("Loading pack items...", PackItemsService.Instance.Load)
+                ];
+
             await Task.Run(() =>
             {
-                Application.Current.Dispatcher.Invoke(() =>
+                for(int i = 0; i < loadingSteps.Length; i++)
                 {
-                    LoadingText.Text = "Loading settings...";
-                });
-                SettingsService.Load();
+                    (string text, Action loader) = loadingSteps[i];
 
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading defines...";
-                });
-                DefinesService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading strings...";
-                });
-                StringsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading models...";
-                });
-                ModelsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading items...";
-                });
-                ItemsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading movers...";
-                });
-                MoversService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading skills...";
-                });
-                SkillsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading characters...";
-                });
-                CharactersService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading sounds config...";
-                });
-                SoundsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading texts...";
-                });
-                TextsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading giftboxes...";
-                });
-                GiftBoxesService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading exchanges...";
-                });
-                ExchangesService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading honors...";
-                });
-                HonorsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading motions...";
-                });
-                MotionsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading accessories...";
-                });
-                AccessoriesService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading couple configuration...";
-                });
-                CoupleService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading tickets...";
-                });
-                TicketsService.Instance.Load();
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    LoadingText.Text = "Loading pack items...";
-                });
-                PackItemsService.Instance.Load();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        LoadingText.Text = text;
+                    });
+                    loader();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        taskbarItemInfo.ProgressValue = (i + 1) / (double)loadingSteps.Length;
+                    });
+                }
 
                 ChangesTrackerService.Instance.Init();
             }).ConfigureAwait(true);
