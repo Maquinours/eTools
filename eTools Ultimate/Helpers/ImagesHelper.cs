b5ac0eb0ead09a451d427306b9cf141d86fb8642
Maquinours <juju9559@hotmail.fr>
2025-06-26 10:47:44 +0200

    Refactor menu icon creation and simplify MainWindowViewModel
    Extracted high-quality image icon creation to a new ImagesHelper class for reuse. Refactored MainWindowViewModel to use readonly arrays for menu items, removed ObservableCollection and related properties, and updated icon creation to use the new helper.

---
+++ eTools Ultimate/Helpers/ImagesHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Wpf.Ui.Controls;
+
+namespace eTools_Ultimate.Helpers
+{
+    public static class ImagesHelper
+    {
+        public static ImageIcon CreateHighQualityImageIcon(string relativePath, int width, int height)
+        {
+            BitmapImage bmp = new();
+            bmp.BeginInit();
+            bmp.UriSource = new Uri($"pack://application:,,,/{relativePath}", UriKind.Absolute);
+            bmp.CacheOption = BitmapCacheOption.OnLoad;
+            bmp.EndInit();
+
+            var image = new ImageIcon
+            {
+                Source = bmp,
+                Width = width,
+                Height = height
+            };
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+
+            return image;
+        }
+    }
+}
--- eTools Ultimate/ViewModels/Windows/MainWindowViewModel.cs
+++ eTools Ultimate/ViewModels/Windows/MainWindowViewModel.cs
@@ -4,59 +4,26 @@ using Wpf.Ui.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System;
+using eTools_Ultimate.Helpers;
 
 namespace eTools_Ultimate.ViewModels.Windows
 {
     public partial class MainWindowViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private string _applicationTitle = "eTools Ultimate";
+        private readonly string _applicationTitle = "eTools Ultimate";
 
-        // Helper-Methode zum Erstellen von ImageIcon mit hoher Qualität
-        private static ImageIcon CreateHighQualityImageIcon(string relativePath, int width, int height)
-        {
-            var packUri = "pack://application:,,,/" + relativePath;
-            var bmp = new BitmapImage();
-            bmp.BeginInit();
-            bmp.UriSource = new Uri(packUri, UriKind.Absolute);
-            bmp.CacheOption = BitmapCacheOption.OnLoad;
-            bmp.EndInit();
-            
-            var image = new ImageIcon
-            {
-                Source = bmp,
-                Width = width,
-                Height = height
-            };
-            
-            // Setze die Renderoptionen für hohe Qualität
-            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
-            
-            return image;
-        }
-
-        public MainWindowViewModel()
-        {
-            // Initialisiere die Menüs mit hochwertigen Icons
-            InitializeMenus();
-        }
-
-        private void InitializeMenus()
-        {
-            // Menüeinträge mit hochwertigen Icons erstellen
-            _menuItems = new ObservableCollection<object>
-            {
-                new NavigationViewItem()
+        private readonly object[] _menuItems = [
+            new NavigationViewItem()
                 {
                     Content = "Dashboard",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/eTools.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/eTools.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.DashboardPage),
                     MenuItemsSource = new object[]
                     {
-                        new NavigationViewItem() 
+                        new NavigationViewItem()
                         {
                             Content = "Change Log",
-                            Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/World.png", 25, 25),
+                            Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/World.png", 25, 25),
                             TargetPageType = typeof(eTools.Views.Pages.ChangeLog.ChangeLogView)
                         }
                     }
@@ -65,96 +32,94 @@ namespace eTools_Ultimate.ViewModels.Windows
                 new NavigationViewItem()
                 {
                     Content = "Items",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Items.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Items.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.ItemPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Movers",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Movers.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Movers.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.MoverPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Skills",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Skills.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Skills.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.SkillPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Motions",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Motions.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Motions.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.Motion.MotionPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Quests",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Quests.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Quests.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.QuestPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "NPCs",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Characters.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Characters.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.NpcPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Events",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Events.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Events.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.EventPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Giftboxes",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Giftboxes.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Giftboxes.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.GiftboxPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Collectors",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Collector.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Collector.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.CollectorPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Accessories",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Accessories.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Accessories.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.Accessory.AccessoryPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Exchangers",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Exchangers.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Exchangers.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.ExchangerPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Honor List",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Honors.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Honors.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.Honor.HonorPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Text Client",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Texts.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Texts.png", 25, 25),
                     TargetPageType = typeof(Views.Pages.TextClientPage)
                 },
                 new NavigationViewItem()
                 {
                     Content = "Couple",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Accessories.png", 25, 25),
-                    TargetPageType = typeof(Views.Pages.CouplePage)
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Accessories.png", 25, 25),
+                    TargetPageType = typeof(Views.Pages.CouplePage),
                 }
-            };
+            ];
 
-            // Footer-Menüs mit hochwertigen Icons erstellen
-            _footerMenuItems = new ObservableCollection<object>
-            {
-                new NavigationViewItem
+        private readonly object[] _footerMenuItems = [
+            new NavigationViewItem
                 {
                     Content = "Settings",
-                    Icon = CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Settings.png", 25, 25),
+                    Icon = ImagesHelper.CreateHighQualityImageIcon("eTools Ultimate;component/Assets/Icons/Settings.png", 25, 25),
                     TargetPageType = typeof(SettingsPage),
                     MenuItemsSource = new object[]
                     {
@@ -163,19 +128,10 @@ namespace eTools_Ultimate.ViewModels.Windows
                         new NavigationViewItem("About", typeof(AboutPage))
                     }
                 }
-            };
-        }
+            ];
 
-        [ObservableProperty]
-        private ObservableCollection<object> _menuItems = new();
-
-        [ObservableProperty]
-        private ObservableCollection<object> _footerMenuItems = new();
-
-        [ObservableProperty]
-        private ObservableCollection<MenuItem> _trayMenuItems = new()
-        {
-            new MenuItem { Header = "Home", Tag = "tray_home" }
-        };
+        public string ApplicationTitle => _applicationTitle;
+        public object[] MenuItems => _menuItems;
+        public object[] FooterMenuItems => _footerMenuItems;
     }
 }
