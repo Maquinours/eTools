7a29e2d45f881e4a3c9d64ea47f7d72716a9d613
Maquinours <juju9559@hotmail.fr>
2025-08-07 16:12:59 +0200

    Select first item on TerrainsTreeView load
    Added TerrainsTreeView_Loaded event handler to automatically select the first item when the TreeView is loaded. This improves user experience by ensuring an item is selected by default.

--- eTools Ultimate/Views/Pages/TerrainObject/TerrainObjectPage.xaml.cs
+++ eTools Ultimate/Views/Pages/TerrainObject/TerrainObjectPage.xaml.cs
@@ -1,4 +1,5 @@
 using eTools_Ultimate.ViewModels.Pages;
+using GongSolutions.Wpf.DragDrop.Utilities;
 using System.Windows.Controls;
 
 namespace eTools_Ultimate.Views.Pages.TerrainObject
@@ -10,6 +11,16 @@ namespace eTools_Ultimate.Views.Pages.TerrainObject
             DataContext = viewModel;
 
             InitializeComponent();
+        }
+
+        private void TerrainsTreeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(sender is not TreeView terrainsTreeView)
+                throw new InvalidOperationException("TerrainsTreeView_Loaded: sender is not a TreeView");
+
+            if (terrainsTreeView.Items.Count > 0)
+                terrainsTreeView.SetSelectedItem(terrainsTreeView.Items[0]);
+
         }
     }
 } 
