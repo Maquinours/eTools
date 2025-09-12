bd99bdea5d533cd640733451005c7b8d3fabf896
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-09-12 10:35:28 +0200

    PersonalizationViewModel rework with localizers

--- eTools Ultimate/ViewModels/Pages/Settings/PersonalizationViewModel.cs
+++ eTools Ultimate/ViewModels/Pages/Settings/PersonalizationViewModel.cs
@@ -15,16 +15,11 @@ using Wpf.Ui.Extensions;
 
 namespace eTools_Ultimate.ViewModels.Pages
 {
-    public partial class PersonalizationViewModel : ObservableObject, INavigationAware
+    public partial class PersonalizationViewModel(IStringLocalizer stringLocalizer, ILocalizationCultureManager cultureManager, IContentDialogService contentDialogService, AppConfig appConfig) : ObservableObject, INavigationAware
     {
         private bool _isInitialized = false;
 
-        private readonly IStringLocalizer _stringLocalizer;
-        private readonly ILocalizationCultureManager _cultureManager;
-        private readonly IContentDialogService _contentDialogService;
-        private readonly AppConfig _appConfig;
-
-        public AppConfig AppConfig => _appConfig;
+        public AppConfig AppConfig => appConfig;
 
         public string DefaultCultureOptionLabel
         {
@@ -37,44 +32,11 @@ namespace eTools_Ultimate.ViewModels.Pages
                 };
 
                 var oldUiCulture = Thread.CurrentThread.CurrentUICulture;
-                Thread.CurrentThread.CurrentUICulture = _cultureManager.GetCulture();
+                Thread.CurrentThread.CurrentUICulture = cultureManager.GetCulture();
                 string defaultCultureName = new CultureInfo(ci.TwoLetterISOLanguageName).DisplayName;
                 Thread.CurrentThread.CurrentUICulture = oldUiCulture;
 
-                return $"{_stringLocalizer["System language"]} ({defaultCultureName})";
-            }
-        }
-
-        public PersonalizationViewModel(AppConfig appConfig, IStringLocalizer stringLocalizer, ILocalizationCultureManager cultureManager, IContentDialogService contentDialogService)
-        {
-            _appConfig = appConfig;
-            _stringLocalizer = stringLocalizer;
-            _cultureManager = cultureManager;
-            _contentDialogService = contentDialogService;
-
-            AppConfig.PropertyChanged += AppConfigService_PropertyChanged;
-        }
-
-        private async void AppConfigService_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            switch (e.PropertyName)
-            {
-                case nameof(AppConfig.Language):
-                    ContentDialogResult result = await _contentDialogService.ShowSimpleDialogAsync(
-                        new SimpleContentDialogCreateOptions()
-                        {
-                            Title = "Application language changed",
-                            Content = "The application needs to be restarted for the language change to take effect.",
-                            PrimaryButtonText = "Restart",
-                            CloseButtonText = "Restart later",
-                        }
-                );
-                    if (result == ContentDialogResult.Primary)
-                    {
-                        System.Diagnostics.Process.Start(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-                        App.Current.Shutdown();
-                    }
-                    break;
+                return $"{stringLocalizer["System default"]} ({defaultCultureName})";
             }
         }
 
@@ -90,7 +52,32 @@ namespace eTools_Ultimate.ViewModels.Pages
 
         private void InitializeViewModel()
         {
+            AppConfig.PropertyChanged += AppConfigService_PropertyChanged;
+
             _isInitialized = true;
+        }
+
+        private async void AppConfigService_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(AppConfig.Language):
+                    ContentDialogResult result = await contentDialogService.ShowSimpleDialogAsync(
+                        new SimpleContentDialogCreateOptions()
+                        {
+                            Title = stringLocalizer["Application language changed"],
+                            Content = stringLocalizer["The application needs to be restarted for the language change to take effect."],
+                            PrimaryButtonText = stringLocalizer["Restart"],
+                            CloseButtonText = stringLocalizer["Restart later"],
+                        }
+                );
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        System.Diagnostics.Process.Start(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                        App.Current.Shutdown();
+                    }
+                    break;
+            }
         }
     }
 }
