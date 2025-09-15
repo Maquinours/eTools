059c397133bb63bef0c1353c9568089ce7dd0437
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-09-15 16:55:35 +0200

    Text identifier validation

---
+++ eTools Ultimate/Helpers/TextIdentifierValidationRule.cs
@@ -0,0 +1,35 @@
+﻿using eTools_Ultimate.Resources;
+using eTools_Ultimate.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace eTools_Ultimate.Helpers
+{
+    internal class TextIdentifierValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is not string identifier)
+                throw new InvalidOperationException("TextIdentifierValidationRule::Validate exception : value is not a string");
+
+            TextsService textsService = App.Services.GetRequiredService<TextsService>();
+            IStringLocalizer<Translations> localizer = App.Services.GetRequiredService<IStringLocalizer<Translations>>();
+
+            ICollectionView textsView = CollectionViewSource.GetDefaultView(textsService.Texts);
+
+            if (textsService.Texts.Any(x => x.Identifier == identifier && x != textsView.CurrentItem))
+                return new ValidationResult(false, $"This identifier is already taken by another text.");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
