2e835cad86dd5dff625b830f128d0ff34d3ae057
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-10-03 10:44:23 +0200

    Add ArrayToVisibilityConverter and update patch notes UI
    Introduced ArrayToVisibilityConverter for improved visibility binding in patch notes dialog. Updated PatchNotesDialog.xaml to use the new converter for patch categories, refactored resource strings for patch notes and translations, and clarified configuration labels in settings and error windows for better localization and usability.

---
+++ eTools Ultimate/Converters/ArrayToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace eTools_Ultimate.Converters
+{
+    public class ArrayToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = value as ICollection;
+            return (collection == null || collection.Count == 0)
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
