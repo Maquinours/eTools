9d2b91691d5f26a625901a800f9d0db561d4120d
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-10-06 10:56:00 +0200

    AvailableUpdateWindow use current theme

--- eTools Ultimate/Views/Windows/AvailableUpdateWindow.xaml.cs
+++ eTools Ultimate/Views/Windows/AvailableUpdateWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using eTools_Ultimate.Models;
+using eTools_Ultimate.ViewModels.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,16 +14,13 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Media.Animation;
-using System.Reflection;
 using Velopack;
 using Velopack.Sources;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
-using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.DependencyInjection;
-using eTools_Ultimate.ViewModels.Windows;
 
 namespace eTools_Ultimate.Views.Windows
 {
@@ -32,11 +34,36 @@ namespace eTools_Ultimate.Views.Windows
         {
             InitializeComponent();
             DataContext = new AvailableUpdateWindowViewModel(update);
+
+            SetTheme();
+            App.Services.GetRequiredService<AppConfig>().PropertyChanged += AppConfig_PropertyChanged;
         }
 
         private void LaterButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void SetTheme()
+        {
+            AppConfig appConfig = App.Services.GetRequiredService<AppConfig>();
+
+            if (IsLoaded)
+                SystemThemeWatcher.UnWatch(this);
+
+            if (appConfig.Theme.HasValue)
+                ApplicationThemeManager.Apply(appConfig.Theme.Value);
+            else
+            {
+                ApplicationThemeManager.ApplySystemTheme();
+                SystemThemeWatcher.Watch(this);
+            }
+        }
+
+        private void AppConfig_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(AppConfig.Theme))
+                SetTheme();
         }
     }
 }
