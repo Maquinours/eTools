457fd8c40e0a2028df7f26672bdf5fe2369a39b4
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-11-26 17:02:22 +0100

    MoverDropList optimization & virtualization fixes

--- eTools Ultimate/ViewModels/Controls/Dialogs/MoverDropListDialogViewModel.cs
+++ eTools Ultimate/ViewModels/Controls/Dialogs/MoverDropListDialogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using eTools_Ultimate.Models;
+using eTools_Ultimate.Models.Items;
 using eTools_Ultimate.Models.Movers;
 using eTools_Ultimate.Resources;
 using eTools_Ultimate.Services;
@@ -13,13 +14,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Threading;
 
 namespace eTools_Ultimate.ViewModels.Controls.Dialogs
 {
     public partial class MoverDropListDialogViewModel : ObservableObject
     {
         private readonly Mover _mover;
-        private WpfObservableRangeCollection<IMoverDrop> _dropList;
+        private WpfObservableRangeCollection<MoverDropTreeViewItem> _dropList;
 
         private DefinesService _definesService = App.Services.GetRequiredService<DefinesService>();
 
@@ -31,7 +33,11 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
         public ICollectionView DropListView => new ListCollectionView(_dropList);
         public List<KeyValuePair<int, string>> ItemIdentifiers => [.. _definesService.ReversedItemDefines];
         public List<KeyValuePair<int, string>> ItemKind3Identifiers => [.. _definesService.ReversedItemKind3Defines];
-        public IEnumerable<IMoverDrop> ComputedDrops => [.. _mover.DropItemGenerator.DropGolds, .. _mover.DropKindGenerator.DropKinds, .. _mover.DropItemGenerator.DropItems];
+        public IEnumerable<MoverDropTreeViewItem> ComputedDrops => [
+            .. _mover.DropItemGenerator.DropGolds.Select(x => new DropGoldTreeViewItem(x)),
+            .. _mover.DropKindGenerator.DropKinds.Select(x => new DropKindTreeViewItem(x)),
+            .. _mover.DropItemGenerator.DropItems.Select(x => new DropItemTreeViewItem(x))
+            ];
 
         public MoverDropListDialogViewModel(Mover mover)
         {
@@ -41,6 +47,42 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
             _mover.DropItemGenerator.DropGolds.CollectionChanged += DropGolds_CollectionChanged;
             _mover.DropItemGenerator.DropItems.CollectionChanged += DropItems_CollectionChanged;
             _mover.DropKindGenerator.DropKinds.CollectionChanged += DropKinds_CollectionChanged;
+            _dropList.CollectionChanged += _dropList_CollectionChanged;
+            foreach(MoverDropTreeViewItem dropItem in _dropList)
+                dropItem.PropertyChanged += DropItem_PropertyChanged;
+        }
+
+        private void DropItem_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (sender is not MoverDropTreeViewItem changedItem)
+                throw new InvalidOperationException("sender is not MoverDropTreeViewItem");
+
+            switch(e.PropertyName)
+            {
+                case nameof(MoverDropTreeViewItem.IsSelected):
+                    if (changedItem.IsSelected == true)
+                    {
+                        foreach (MoverDropTreeViewItem item in _dropList.Where(x => x != sender))
+                            item.IsSelected = false;
+                    }
+                    break;
+            }
+        }
+
+        private void _dropList_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if(e.NewItems != null)
+            {
+                MoverDropTreeViewItem[] newItems = [..e.NewItems.Cast<MoverDropTreeViewItem>()];
+                foreach (MoverDropTreeViewItem newItem in newItems)
+                    newItem.PropertyChanged += DropItem_PropertyChanged;
+            }
+            if(e.OldItems != null)
+            {
+                MoverDropTreeViewItem[] oldItems = [.. e.OldItems.Cast<MoverDropTreeViewItem>()];
+                foreach (MoverDropTreeViewItem newItem in oldItems)
+                    newItem.PropertyChanged -= DropItem_PropertyChanged;
+            }
         }
 
         [RelayCommand]
@@ -50,7 +92,7 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
 
             _mover.DropKindGenerator.DropKinds.Add(dropKind);
 
-            DropAdded?.Invoke(this, new(dropKind));
+            DropAdded?.Invoke(this, new(new DropKindTreeViewItem(dropKind)));
         }
 
         [RelayCommand]
@@ -60,7 +102,7 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
 
             _mover.DropItemGenerator.DropItems.Add(dropItem);
 
-            DropAdded?.Invoke(this, new(dropItem));
+            DropAdded?.Invoke(this, new(new DropItemTreeViewItem(dropItem)));
         }
 
         [RelayCommand]
@@ -70,7 +112,7 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
 
             _mover.DropItemGenerator.DropGolds.Add(dropGold);
 
-            DropAdded?.Invoke(this, new(dropGold));
+            DropAdded?.Invoke(this, new(new DropGoldTreeViewItem(dropGold)));
         }
 
         [RelayCommand(CanExecute = nameof(CanRemoveDrop))]
@@ -78,64 +120,122 @@ namespace eTools_Ultimate.ViewModels.Controls.Dialogs
         {
             switch (parameter)
             {
-                case DropItem dropItem:
-                    _mover.DropItemGenerator.DropItems.Remove(dropItem);
+                case DropItemTreeViewItem dropItem:
+                    _mover.DropItemGenerator.DropItems.Remove(dropItem.DropItem);
                     break;
-                case DropGold dropGold:
-                    _mover.DropItemGenerator.DropGolds.Remove(dropGold);
+                case DropGoldTreeViewItem dropGold:
+                    _mover.DropItemGenerator.DropGolds.Remove(dropGold.DropGold);
                     break;
-                case DropKind dropKind:
-                    _mover.DropKindGenerator.DropKinds.Remove(dropKind);
+                case DropKindTreeViewItem dropKind:
+                    _mover.DropKindGenerator.DropKinds.Remove(dropKind.DropKind);
                     break;
                 default:
                     throw new InvalidOperationException("drop is not a valid class");
             }
         }
 
-        private static bool CanRemoveDrop(object? parameter) => parameter is DropGold or DropKind or DropItem;
+        private static bool CanRemoveDrop(object? parameter) => parameter is DropGoldTreeViewItem or DropKindTreeViewItem or DropItemTreeViewItem;
 
         private void DropKinds_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.NewItems is not null)
                 _dropList.InsertRange(
-                    (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropKind || x.item is DropGold)?.index ?? -1) + 1,
-                    e.NewItems.Cast<DropKind>());
-            if (e.OldItems is not null)
-                _dropList.RemoveRange(e.OldItems.Cast<DropKind>());
+                    (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropKindTreeViewItem || x.item is DropGoldTreeViewItem)?.index ?? -1) + 1,
+                    [..e.NewItems.Cast<DropKind>().Select(x => new DropKindTreeViewItem(x))]
+                    );
+            if (e.OldItems is not null) 
+            {
+                DropKind[] oldItems = [.. e.OldItems.Cast<DropKind>()];
+                _dropList.RemoveAll(x => x is DropKindTreeViewItem item &&  oldItems.Any(y => item.DropKind == y));
+            }
 
-            if (!_dropList.SequenceEqual(ComputedDrops))
-                throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
+            //if (!_dropList.SequenceEqual(ComputedDrops))
+            //    throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
         }
 
         private void DropItems_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
-            if (e.NewItems is not null)
-                _dropList.InsertRange(
-                    (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropItem || x.item is DropKind || x.item is DropGold)?.index ?? -1) + 1,
-                    e.NewItems.Cast<DropItem>());
-            if (e.OldItems is not null)
-                _dropList.RemoveRange(e.OldItems.Cast<DropItem>());
+            App.Current.Dispatcher.BeginInvoke(() =>
+            {
+                if (e.NewItems is not null)
+                    _dropList.InsertRange(
+                        (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropItemTreeViewItem || x.item is DropKindTreeViewItem || x.item is DropGoldTreeViewItem)?.index ?? -1) + 1,
+                        [.. e.NewItems.Cast<DropItem>().Select(x => new DropItemTreeViewItem(x))]
+                        );
+                if (e.OldItems is not null)
+                {
+                    DropItem[] oldItems = [.. e.OldItems.Cast<DropItem>()];
+                    _dropList.RemoveAll(x => x is DropItemTreeViewItem item && oldItems.Any(y => item.DropItem == y));
+                }
+            });
 
-            if (!_dropList.SequenceEqual(ComputedDrops))
-                throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
+            //if (!_dropList.SequenceEqual(ComputedDrops))
+            //    throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
         }
 
         private void DropGolds_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.NewItems is not null)
                 _dropList.InsertRange(
-                    (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropGold)?.index ?? -1) + 1,
-                    e.NewItems.Cast<DropGold>());
+                    (_dropList.Select((item, index) => new { item, index }).LastOrDefault(x => x.item is DropGoldTreeViewItem)?.index ?? -1) + 1,
+                    [..e.NewItems.Cast<DropGold>().Select(x => new DropGoldTreeViewItem(x))]
+                    );
             if (e.OldItems is not null)
-                _dropList.RemoveRange(e.OldItems.Cast<DropGold>());
+            {
+                DropGold[] oldItems = [.. e.OldItems.Cast<DropGold>()];
+                _dropList.RemoveAll(x => x is DropGoldTreeViewItem item && oldItems.Any(y => item.DropGold == y));
+            }
 
-            if (!_dropList.SequenceEqual(ComputedDrops))
-                throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
+            //if (!_dropList.SequenceEqual(ComputedDrops))
+            //    throw new InvalidOperationException("dropList is not sequence equal to ComputedDrops");
         }
     }
 
-    public class DropAddedEventArgs(IMoverDrop drop)
+    public partial class MoverDropTreeViewItem : ObservableObject
     {
-        public IMoverDrop Drop { get; } = drop;
+        [ObservableProperty]
+        private bool _isSelected = false;
+    }
+
+    public class DropGoldTreeViewItem(DropGold dropGold) : MoverDropTreeViewItem
+    {
+        public DropGold DropGold { get; } = dropGold;
+    }
+
+    public class DropItemTreeViewItem(DropItem dropItem) : MoverDropTreeViewItem
+    {
+        public DropItem DropItem { get; } = dropItem;
+    }
+
+    public class DropKindTreeViewItem : MoverDropTreeViewItem, INotifyPropertyChanged
+    {
+
+        public DropKind DropKind { get; }
+        public ItemTreeViewItem[] DropItems => [.. DropKind.Items.Select(x => new ItemTreeViewItem(x))];
+
+        public DropKindTreeViewItem(DropKind dropKind)
+        {
+            DropKind = dropKind;
+            DropKind.PropertyChanged += DropKind_PropertyChanged;
+        }
+
+        private void DropKind_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            switch(e.PropertyName)
+            {
+                case nameof(DropKind.Items):
+                    OnPropertyChanged(nameof(DropItems));
+                    break;
+            }
+        }
+    }
+    public class ItemTreeViewItem(Item item) : MoverDropTreeViewItem
+    {
+        public Item Item { get; } = item;
+    }
+
+    public class DropAddedEventArgs(MoverDropTreeViewItem dropTreeItem)
+    {
+        public MoverDropTreeViewItem DropTreeItem { get; } = dropTreeItem;
     }
 }
--- eTools Ultimate/Views/Dialogs/MoverDropListDialog.xaml.cs
+++ eTools Ultimate/Views/Dialogs/MoverDropListDialog.xaml.cs
@@ -2,6 +2,7 @@
 using eTools_Ultimate.ViewModels.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,12 +32,246 @@ namespace eTools_Ultimate.Views.Dialogs
             InitializeComponent();
 
             viewModel.DropAdded += ViewModel_DropAdded;
+            viewModel.DropListView.CollectionChanged += ViewModel_DropListView_CollectionChanged;
+        }
+
+        private void ViewModel_DropListView_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch(e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                    {
+                        if (e.NewItems is not null && e.NewItems.Count > 0)
+                        {
+                            IEnumerable<MoverDropTreeViewItem> newItems = e.NewItems.Cast<MoverDropTreeViewItem>();
+                            MoverDropTreeViewItem lastNewItem = newItems.Last();
+
+                            Application.Current.Dispatcher.Invoke(async () =>
+                            {
+                                GetTreeViewItem(DropListTreeView, lastNewItem);
+                                lastNewItem.IsSelected = true;
+                                //DropListTreeView.Items.Cast<MoverDropTreeViewItem>().ToList().ForEach(item => item.IsExpanded = true);
+                            }, System.Windows.Threading.DispatcherPriority.DataBind);
+                        }
+                        break;
+                    }
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+                    {
+                        if(e.OldItems is not null && e.OldItems.Count > 0)
+                        {
+                            Application.Current.Dispatcher.Invoke(async () =>
+                            {
+                                MoverDropTreeViewItem[] items = [..DropListTreeView.Items.Cast<MoverDropTreeViewItem>()];
+
+                                MoverDropTreeViewItem? newSelectedItem = null;
+
+                                if (items.Length > e.OldStartingIndex)
+                                    newSelectedItem = items[e.OldStartingIndex];
+                                else if (items.Length > 0)
+                                    newSelectedItem = items[^1];
+
+                                if (newSelectedItem != null)
+                                {
+                                    GetTreeViewItem(DropListTreeView, newSelectedItem);
+                                    newSelectedItem.IsSelected = true;
+                                }
+                            }, System.Windows.Threading.DispatcherPriority.DataBind);
+                        }
+                        break;
+                    }
+            }
+            
         }
 
         private void ViewModel_DropAdded(object? sender, DropAddedEventArgs e)
         {
-            System.Windows.Controls.TreeViewItem? tvi = FindTviFromObjectRecursive(DropListTreeView, e.Drop);
-            if (tvi != null) tvi.IsSelected = true;
+            //Application.Current.Dispatcher.Invoke(async () =>
+            //{
+            //    GetTreeViewItem(DropListTreeView, e.DropTreeItem);
+            //    e.DropTreeItem.IsSelected = true;
+            //}, System.Windows.Threading.DispatcherPriority.Render);
+        }
+
+        private System.Windows.Controls.TreeViewItem? GetTreeViewItem(TreeView treeView, object item)
+        {
+            // Try to generate the ItemsPresenter and the ItemsPanel.
+            // by calling ApplyTemplate.  Note that in the
+            // virtualizing case even if the item is marked
+            // expanded we still need to do this step in order to
+            // regenerate the visuals because they may have been virtualized away.
+            treeView.ApplyTemplate();
+            ItemsPresenter? itemsPresenter =
+                (ItemsPresenter)treeView.Template.FindName("ItemsHost", treeView);
+            if (itemsPresenter != null)
+            {
+                itemsPresenter.ApplyTemplate();
+            }
+            else
+            {
+                // The Tree template has not named the ItemsPresenter,
+                // so walk the descendents and find the child.
+                itemsPresenter = FindVisualChild<ItemsPresenter>(treeView);
+                if (itemsPresenter == null)
+                {
+                    treeView.UpdateLayout();
+
+                    itemsPresenter = FindVisualChild<ItemsPresenter>(treeView);
+                }
+            }
+
+            Panel itemsHostPanel = (Panel)VisualTreeHelper.GetChild(itemsPresenter, 0);
+
+            // Ensure that the generator for this panel has been created.
+            UIElementCollection children = itemsHostPanel.Children;
+
+            if (itemsHostPanel is MyVirtualizingStackPanel virtualizingPanel)
+            {
+                int itemIndex = treeView.Items.Cast<object>().ToList().IndexOf(item);
+                if (itemIndex != -1)
+                {
+                    virtualizingPanel.BringIntoView(itemIndex);
+                    return (System.Windows.Controls.TreeViewItem)treeView.ItemContainerGenerator.
+                            ContainerFromIndex(itemIndex);
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Recursively search for an item in this subtree.
+        /// </summary>
+        /// <param name="container">
+        /// The parent ItemsControl. This can be a TreeView or a TreeViewItem.
+        /// </param>
+        /// <param name="item">
+        /// The item to search for.
+        /// </param>
+        /// <returns>
+        /// The TreeViewItem that contains the specified item.
+        /// </returns>
+        //private System.Windows.Controls.TreeViewItem? GetTreeViewItem(ItemsControl container, object item)
+        //{
+        //    if (container != null)
+        //    {
+        //        if (container.DataContext == item)
+        //        {
+        //            return container as System.Windows.Controls.TreeViewItem;
+        //        }
+
+        //        // Expand the current container
+        //        if (container is System.Windows.Controls.TreeViewItem && !((System.Windows.Controls.TreeViewItem)container).IsExpanded)
+        //        {
+        //            container.SetValue(System.Windows.Controls.TreeViewItem.IsExpandedProperty, true);
+        //        }
+
+        //        // Try to generate the ItemsPresenter and the ItemsPanel.
+        //        // by calling ApplyTemplate.  Note that in the
+        //        // virtualizing case even if the item is marked
+        //        // expanded we still need to do this step in order to
+        //        // regenerate the visuals because they may have been virtualized away.
+
+        //        container.ApplyTemplate();
+        //        ItemsPresenter? itemsPresenter =
+        //            (ItemsPresenter)container.Template.FindName("ItemsHost", container);
+        //        if (itemsPresenter != null)
+        //        {
+        //            itemsPresenter.ApplyTemplate();
+        //        }
+        //        else
+        //        {
+        //            // The Tree template has not named the ItemsPresenter,
+        //            // so walk the descendents and find the child.
+        //            itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+        //            if (itemsPresenter == null)
+        //            {
+        //                container.UpdateLayout();
+
+        //                itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+        //            }
+        //        }
+
+        //        Panel itemsHostPanel = (Panel)VisualTreeHelper.GetChild(itemsPresenter, 0);
+
+        //        // Ensure that the generator for this panel has been created.
+        //        UIElementCollection children = itemsHostPanel.Children;
+
+        //        MyVirtualizingStackPanel? virtualizingPanel =
+        //            itemsHostPanel as MyVirtualizingStackPanel;
+
+        //        for (int i = 0, count = container.Items.Count; i < count; i++)
+        //        {
+        //            System.Windows.Controls.TreeViewItem subContainer;
+        //            if (virtualizingPanel != null)
+        //            {
+        //                // Bring the item into view so
+        //                // that the container will be generated.
+        //                virtualizingPanel.BringIntoView(i);
+
+        //                subContainer =
+        //                    (System.Windows.Controls.TreeViewItem)container.ItemContainerGenerator.
+        //                    ContainerFromIndex(i);
+        //            }
+        //            else
+        //            {
+        //                subContainer =
+        //                    (System.Windows.Controls.TreeViewItem)container.ItemContainerGenerator.
+        //                    ContainerFromIndex(i);
+
+        //                // Bring the item into view to maintain the
+        //                // same behavior as with a virtualizing panel.
+        //                subContainer.BringIntoView();
+        //            }
+
+        //            if (subContainer != null)
+        //            {
+        //                // Search the next level for the object.
+        //                System.Windows.Controls.TreeViewItem? resultContainer = GetTreeViewItem(subContainer, item);
+        //                if (resultContainer != null)
+        //                {
+        //                    return resultContainer;
+        //                }
+        //                else
+        //                {
+        //                    // The object is not under this TreeViewItem
+        //                    // so collapse it.
+        //                    subContainer.IsExpanded = false;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return null;
+        //}
+
+        /// <summary>
+        /// Search for an element of a certain type in the visual tree.
+        /// </summary>
+        /// <typeparam name="T">The type of element to find.</typeparam>
+        /// <param name="visual">The parent element.</param>
+        /// <returns></returns>
+        private T? FindVisualChild<T>(Visual visual) where T : Visual
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(visual); i++)
+            {
+                Visual child = (Visual)VisualTreeHelper.GetChild(visual, i);
+                if (child != null)
+                {
+                    T? correctlyTyped = child as T;
+                    if (correctlyTyped != null)
+                    {
+                        return correctlyTyped;
+                    }
+
+                    T? descendent = FindVisualChild<T>(child);
+                    if (descendent != null)
+                    {
+                        return descendent;
+                    }
+                }
+            }
+
+            return null;
         }
 
         public static System.Windows.Controls.TreeViewItem? FindTviFromObjectRecursive(ItemsControl ic, object o)
@@ -57,5 +292,17 @@ namespace eTools_Ultimate.Views.Dialogs
             return null;
         }
 
+    }
+
+    public class MyVirtualizingStackPanel : VirtualizingStackPanel
+    {
+        /// <summary>
+        /// Publically expose BringIndexIntoView.
+        /// </summary>
+        public void BringIntoView(int index)
+        {
+
+            this.BringIndexIntoView(index);
+        }
     }
 }
